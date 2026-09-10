2b9651cc5707b3f986cc037108e15a6391c09d78
Damien Joseph Martin <amigosiamoneycard@gmail.com>
2026-09-10 10:43:16 +0530

    feat(mobile): configure staging application id and default backend URL

--- Flutter Money card/android/app/build.gradle.kts
+++ Flutter Money card/android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.moneycard.staff.money_card_staff"
+        applicationId = "com.moneycard.staff.money_card_staff.staging"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
